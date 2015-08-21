7b1e92da8aab38937fb89fe110196c913be90aee
Sam Caldwell <samcaldwell@utexas.edu>
2015-08-21 10:36:24 -0400

    add world_platformer

---
+++ world_platform.rkt
@@ -0,0 +1,54 @@
+#lang racket
+
+(require 2htdp/universe)
+(require 2htdp/image)
+
+(require rackunit)
+
+;; move a circle around the screen in response to arrow key presses
+
+(struct posn (x y) #:transparent)
+
+(define DELTA 10)
+
+(define RADIUS 20)
+
+(define IT (circle RADIUS "solid" "blue"))
+
+;; length of each side of the square
+(define CANVAS-SIZE 400)
+
+;; adjust the xy point representing the center of the circle
+;; so that it stays in the canvas
+(define (adjust xy)
+  (define (adjust-1d x)
+    (cond
+      [(< x RADIUS) RADIUS]
+      [(> x (- CANVAS-SIZE RADIUS)) (- CANVAS-SIZE RADIUS)]
+      [else x]))
+  (posn (adjust-1d (posn-x xy))
+        (adjust-1d (posn-y xy))))
+
+(module+ test
+  (check-equal? (adjust (posn 100 100))
+                (posn 100 100))
+  (check-equal? (adjust (posn 0 100))
+                (posn RADIUS 100))
+  (check-equal? (adjust (posn 100 0))
+                (posn 100 RADIUS))
+  (check-equal? (adjust (posn 0 0))
+                (posn RADIUS RADIUS))
+  (check-equal? (adjust (posn -10 200))
+                (posn RADIUS 200))
+  (check-equal? (adjust (posn 200 -10))
+                (posn 200 RADIUS))
+  (check-equal? (adjust (posn CANVAS-SIZE CANVAS-SIZE))
+                (posn (- CANVAS-SIZE RADIUS) (- CANVAS-SIZE RADIUS)))
+  )
+                    
+(define (key-handler pos key)
+  (match key
+    ['left (adjust (struct-copy posn pos [x (- (posn-x pos) DELTA)]))]
+    ['right (adjust (struct-copy posn pos [x (+ (posn-x pos) DELTA)]))]
+    ['up (adjust (struct-copy posn pos [y (+ (posn-y pos) DELTA)]))]
+    ['down (adjust (struct-copy posn pos [y (- (posn-y pos) DELTA)]))]))
