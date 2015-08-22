172a8e66d0419adb62e8ea0f441b550bc66f2d6a
Sam Caldwell <samcaldwell@utexas.edu>
2015-08-21 22:56:00 -0400

    cron commit

--- prospect_platformer.rkt
+++ prospect_platformer.rkt
@@ -16,13 +16,13 @@
     [else x]))
 
 #;(module+ test
-  (require rackunit)
-  (check-equal? (clip 5 0 0) 0)
-  (check-equal? (clip 0 0 0) 0)
-  (check-equal? (clip -1 10 20) 10)
-  (check-equal? (clip -1 -20 -10) -10)
-  (check-equal? (clip 1 1 2) 1)
-  (check-equal? (clip 2 1 2) 2))
+    (require rackunit)
+    (check-equal? (clip 5 0 0) 0)
+    (check-equal? (clip 0 0 0) 0)
+    (check-equal? (clip -1 10 20) 10)
+    (check-equal? (clip -1 -20 -10) -10)
+    (check-equal? (clip 1 1 2) 1)
+    (check-equal? (clip 2 1 2) 2))
 
 ;; Move a point on a single dimension so that it is within [floor + margin-low, ceil - margin-high].
 ;; If the point is already inside that range, return unchanged.
@@ -31,15 +31,15 @@
   (clip center (+ floor margin-low) (- ceil margin-high)))
 
 #;(module+ test
-  (check-equal? (adjust-1d 5 10 10 0 100)
-                10)
-  (check-equal? (adjust-1d 95 10 10 0 100)
-                90)
-  (check-equal? (adjust-1d 50 10 10 0 100)
-                50)
-  ;; weird case
-  (check-equal? (adjust-1d 50 100 100 0 100)
-                100))
+    (check-equal? (adjust-1d 5 10 10 0 100)
+                  10)
+    (check-equal? (adjust-1d 95 10 10 0 100)
+                  90)
+    (check-equal? (adjust-1d 50 10 10 0 100)
+                  50)
+    ;; weird case
+    (check-equal? (adjust-1d 50 100 100 0 100)
+                  100))
 
 ;; move a shape within a canvas
 ;; the canvas is assumed to be a rectangle with its top left corner at (0,0) and bottom right corner at bot-right-pos
@@ -61,16 +61,16 @@
     [_ (posn 0 0)]))
 
 #;(module+ test
-  (check-equal? (key-to-posn-delta "left" 10)
-                (posn -10 0))
-  (check-equal? (key-to-posn-delta "right" 42)
-                (posn 42 0))
-  (check-equal? (key-to-posn-delta "down" 999)
-                (posn 0 999))
-  (check-equal? (key-to-posn-delta "up" -3)
-                (posn 0 3))
-  (check-equal? (key-to-posn-delta "w" 5)
-                (posn 0 0)))
+    (check-equal? (key-to-posn-delta "left" 10)
+                  (posn -10 0))
+    (check-equal? (key-to-posn-delta "right" 42)
+                  (posn 42 0))
+    (check-equal? (key-to-posn-delta "down" 999)
+                  (posn 0 999))
+    (check-equal? (key-to-posn-delta "up" -3)
+                  (posn 0 3))
+    (check-equal? (key-to-posn-delta "w" 5)
+                  (posn 0 0)))
 
 (define (arrow? key)
   (match key
@@ -98,19 +98,44 @@
 (define CANVAS-SIZE 400)
 (define DELTA 20)
 
-(parameterize ((current-eventspace (make-eventspace)))
-  (define frame (new frame%
-                     [label "My Frame"]
-                     [width CANVAS-SIZE]
-                     [height CANVAS-SIZE]))
-  
-  (define canvas
-    (let* ([circ-radius 20]
-           [circ (box (shape (posn 0 0) (* circ-radius 2) (* circ-radius 2)))])
-      (new game-canvas%
-           [parent frame]
-           [key-handler (lambda (key) (send-ground-message (key-event key)))])))
-#|
+;; it1 and it2 are both shapes
+(struct worldstate (it1 it2) #:transparent)
+
+(define (draw-shape-ellipse dc sh color)
+  (match-define (shape (posn tl-x tl-y) x-size y-size) sh)
+  (send dc set-brush color 'solid)
+  (send dc set-pen color 1 'transparent)
+  (send dc set-smoothing 'aligned)
+  (send dc draw-ellipse tl-x tl-y x-size y-size))
+
+(define (draw-ws ws dc)
+  (match-define (worldstate it1 it2) ws)
+  (send dc suspend-flush)
+  (send dc clear)
+  (draw-shape-ellipse dc it1 'blue)
+  (draw-shape-ellipse dc it2 'red)
+  (send dc resume-flush))
+
+
+(define (make-world dc)
+  (spawn
+   (lambda (e ws)
+     (match e
+       [(message (at-meta (key-event key)))
+        
+        (parameterize ((current-eventspace (make-eventspace)))
+          (define frame (new frame%
+                             [label "My Frame"]
+                             [width CANVAS-SIZE]
+                             [height CANVAS-SIZE]))
+          
+          (define canvas
+            (let* ([circ-radius 20]
+                   [circ (box (shape (posn 0 0) (* circ-radius 2) (* circ-radius 2)))])
+              (new game-canvas%
+                   [parent frame]
+                   [key-handler (lambda (key) (send-ground-message (key-event key)))])))
+          #|
            (define dc (send canvas get-dc))
                           (define-values (max-x max-y) (send canvas get-client-size))
                           (match-define (posn dx dy) (key-to-posn-delta key DELTA))
@@ -122,5 +147,5 @@
                           (send dc set-smoothing 'aligned)
                           (send dc draw-ellipse tl-x tl-y x-size y-size))])))
 |#
-  
-  (send frame show #t))
+          
+          (send frame show #t))
