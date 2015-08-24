43243b6684b8fb4e609c40803fb649da698eb61c
Sam Caldwell <samcaldwell@utexas.edu>
2015-08-24 10:40:00 -0400

    cron commit

--- prospect_moving_dots2.rkt
+++ prospect_moving_dots2.rkt
@@ -176,7 +176,7 @@
     (define bot-right (posn x-max y-max))
     (define dc (send canvas get-dc))
     (spawn-drawer dc)
-    (spawn-dot DOT1 (arrow-keys "w" "a" "s" "d") bot-right)
-    (spawn-dot DOT2 (arrow-keys "up" "left" "down" "right") bot-right)))
+    #;(spawn-dot DOT1 (arrow-keys "w" "a" "s" "d") bot-right)
+    #;(spawn-dot DOT2 (arrow-keys "up" "left" "down" "right") bot-right)))
 
 (make-frame 400 400)
