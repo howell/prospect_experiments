d1d755876dfe18641448f46f6911768b4455c5a4
Sam Caldwell <samcaldwell@utexas.edu>
2015-08-25 11:20:00 -0400

    cron commit

--- prospect_moving_dots3.rkt
+++ prospect_moving_dots3.rkt
@@ -144,8 +144,8 @@
             (define dots-n2 (set-add dots-n new-dot))
             (if (any-colliding? new-dot dots-n)
                 (cons dots-n2 (cons (message `(move ,(shape-l-label new-dot)
-                                                    ,(random-in-range DELTA)
-                                                    ,(random-in-range DELTA)))
+                                                    ,(random-in-range (- DELTA) DELTA)
+                                                    ,(random-in-range (- DELTA) DELTA)))
                                     msgs))
                 (cons dots-n2 msgs))))
         (transition next-dots msgs)]
