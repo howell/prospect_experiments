92f3261d1f702056060c960899859f592d0a13ab
Sam Caldwell <samcaldwell@utexas.edu>
2015-08-25 10:30:00 -0400

    cron commit

--- prospect_moving_dots3.rkt
+++ prospect_moving_dots3.rkt
@@ -1,5 +1,7 @@
 #lang prospect
 
+(require racket/gui racket/draw racket/set)
+
 ;; Protocol description - take 3 (collision detection)
 ;; key board events are injected into the system using messages of the form ('key-event k)
 ;; - each dot process has a unique label P and asserts its current location as ('shape P shape).
@@ -10,3 +12,189 @@
 ;; - When a dot P receives a move message it updates its assertions to reflect the new location
 ;; - a drawing process D listens for every location and draws/erases to the screen as postions
 ;;   are asserted and retracted
+
+(struct posn (x y) #:transparent)
+;; kind of assumed to be rectangular
+(struct shape (top-left x-size y-size color) #:transparent)
+(struct dot-state (my-shape everyone-else) #:transparent)
+
+(struct arrow-keys (up left down right) #:transparent)
+
+(define (key-to-arrow arr-keys key)
+  (match-define (arrow-keys up left down right) arr-keys)
+  (match key
+    [(== up) "up"]
+    [(== left) "left"]
+    [(== right) "right"]
+    [(== down) "down"]
+    [_ (error 'key-to-arrow "expected arrow key")]))
+
+;; clip x to be in the range [min,max]
+(define (clip x min max)
+  (cond
+    [(< x min) min]
+    [(> x max) max]
+    [else x]))
+
+;; move a shape within a canvas
+;; the canvas is assumed to be a rectangle with its top left corner at (0,0) and bottom right corner at bot-right-pos
+;; the shape is moved in the x axis by dx and y axis by dy. The shape is not moved outside of the canvas.
+(define (move-shape-in-canvas shp dx dy bot-right-pos)
+  (match-define (shape (posn tl-x tl-y) x-size y-size c) shp)
+  (match-define (posn x-max y-max) bot-right-pos)
+  (shape (posn (clip (+ tl-x dx) 0 (- x-max x-size))
+               (clip (+ tl-y dy) 0 (- y-max y-size)))
+         x-size y-size c))
+
+;; translates a key press into a directional move - a (posn dx dy)
+(define (key-to-posn-delta key delta)
+  (match key
+    ["left" (posn (- delta) 0)]
+    ["right" (posn delta 0)]
+    ["up" (posn 0 (- delta))]
+    ["down" (posn 0 delta)]
+    [_ (posn 0 0)]))
+
+;; check if two circles with centers p1 p2 and radii r1 r2
+;; are colliding
+(define (colliding-circles? p1 r1 p2 r2)
+  (match-define (posn x1 y1) p1)
+  (match-define (posn x2 y2) p2)
+  (define d^2 (+ (expt (- x1 x2) 2)
+                 (expt (- y1 y2) 2)))
+  (define r^2 (expt (+ r1 r2) 2))
+  (< d^2 r^2))
+
+(define (circle-center sh)
+  (match-define (shape (posn tl-x tl-y) d _ _) sh)
+  (define r (/ d 2))
+  (posn (+ tl-x r) (- tl-y r)))
+
+(define DELTA 20)
+
+(define shape-detector
+  (compile-projection `(shape ,(?!))))
+
+(define (match-shapes m)
+  (for/set [(x (matcher-project/set m shape-detector))]
+    (match-define (list sh) x)
+    sh))
+
+(define (dot-behavior arr-keys bot-right)
+  (match-define (arrow-keys up left down right) arr-keys)
+  (lambda (e s)
+    (match-define (dot-state me others) s)
+    (match e
+      [(patch added removed)
+       ;; update the position of all shapes
+       (define vacated (match-shapes removed))
+       (define moved (match-shapes added))
+       (transition (dot-state me (set-remove (set-union (set-subtract others vacated) moved) me)) '())]
+      [(message (at-meta `(key-event ,key)))
+       (match key
+         [(or (== up) (== left) (== down) (== right))
+          (define arrow-key (key-to-arrow arr-keys key))
+          (match-define (posn dx dy) (key-to-posn-delta arrow-key DELTA))
+          (define moved (move-shape-in-canvas me dx dy bot-right))
+          (define any-colliding? (for/fold [(acc #f)]
+                                           [(other others)]
+                                   (or acc (colliding-circles? (circle-center moved) RADIUS (circle-center other) RADIUS))))
+          (if any-colliding?
+              #f
+              (transition (dot-state moved others)
+                          (patch-seq (retract `(shape ,?))
+                                     (assert `(shape ,moved)))))]
+         [_ #f])]
+      [_ #f])))
+
+(define (spawn-dot shape keys bot-right)
+  (spawn
+   (dot-behavior keys bot-right)
+   (dot-state shape (set))
+   (assert `(shape ,shape))
+   (sub `(shape ,?))
+   (sub `(key-event ,?) #:meta-level 1)))
+
+(define (draw-shape-ellipse dc sh smoothing)
+  (match-define (shape (posn tl-x tl-y) x-size y-size color) sh)
+  (send dc set-brush color 'solid)
+  (send dc set-pen color 1 'solid)
+  (send dc set-smoothing smoothing)
+  (send dc draw-ellipse tl-x tl-y x-size y-size))
+
+(define (draw-shapes dc shapes)
+  (send dc suspend-flush)
+  (send dc clear)
+  (for ([sh shapes])
+    (draw-shape-ellipse dc sh 'aligned))
+  (send dc resume-flush))
+
+(define (update-canvas dc added removed)
+  (define background (send dc get-background))
+  (send dc suspend-flush)
+  (for ([sh removed])
+    (draw-shape-ellipse dc (struct-copy shape sh [color background]) 'unsmoothed))
+  (for ([sh added])
+    (draw-shape-ellipse dc sh 'aligned))
+  (send dc resume-flush))
+
+(define (spawn-drawer dc)
+  (spawn
+   (lambda (e s)
+     (match e
+       [(patch added removed)
+        (define vacated (match-shapes removed))
+        (define moved (match-shapes added))
+        (update-canvas dc moved vacated)
+        #f]
+       [_ #f]))
+   (void)
+   (sub `(shape ,?))))
+
+;; gui stuff
+(define game-canvas%
+  (class canvas%
+    (init-field key-handler)
+    (define/override (on-char event)
+      (define key-code (send event get-key-code))
+      (cond
+        [(char? key-code) (key-handler (string key-code))]
+        [(arrow? key-code) (key-handler (symbol->string key-code))]
+        [else (void)]))
+    (super-new)))
+
+(define RADIUS 20)
+(define DOT1 (shape (posn 0 0) (* RADIUS 2) (* RADIUS 2) "blue"))
+(define DOT2 (shape (posn 340 340) (* RADIUS 2) (* RADIUS 2) "red"))
+(define DOT3 (shape (posn 0 340) (* RADIUS 2) (* RADIUS 2) "green"))
+(define DOT4 (shape (posn 340 0) (* RADIUS 2) (* RADIUS 2) "purple"))
+
+(define (arrow? key)
+  (match key
+    [(or 'left 'right 'up 'down) #t]
+    [_ #f]))
+
+(define bot-right (box #f))
+
+(define (make-frame width height)
+  (parameterize ((current-eventspace (make-eventspace)))
+    (define frame (new frame%
+                       [label "My Frame"]
+                       [width width]
+                       [height height]))
+    (define canvas
+      (new game-canvas%
+           [parent frame]
+           [key-handler (lambda (key) (send-ground-message `(key-event ,key)))]))
+    (send frame show #t)
+    (define-values (x-max y-max) (send canvas get-client-size))
+    (set-box! bot-right (posn x-max y-max))
+    (define dc (send canvas get-dc))
+    (spawn-drawer dc)))
+
+(make-frame 400 400)
+
+(spawn-dot DOT1 (arrow-keys "w" "a" "s" "d") (unbox bot-right))
+(spawn-dot DOT2 (arrow-keys "up" "left" "down" "right") (unbox bot-right))
+(spawn-dot DOT3 (arrow-keys "k" "h" "j" "l") (unbox bot-right))
+(spawn-dot DOT4 (arrow-keys "i" "y" "u" "o") (unbox bot-right))
