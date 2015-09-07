9c65791f3507a9e798b66ee382d6bae9e1716dd8
Sam Caldwell <samcaldwell@utexas.edu>
2015-09-07 16:50:01 -0400

    cron commit

--- prospect_platformer.rkt
+++ prospect_platformer.rkt
@@ -5,6 +5,24 @@
 ;; the game logic process listens for movements and asserts the actual locations
 ;; a rendering process listens for the actual locations and draws the map accordingly
 
-(require "./geometry.rkt")
+(require "./geometry.rkt"
+         rackunit)
 
 (struct rect (top-left width height) #:transparent)
+
+(struct motion (x y dx dy ddx ddy))
+
+;; rect motion [listof rect] -> rect
+;;
+(define (move r m lor)
+  r)
+
+(check-equal? (move (rect (posn 0 0) 1 1)
+                    (motion 1 1 0 0 0 0)
+                    '())
+              (rect (posn 1 1) 1 1))
+
+(check-equal? (move (rect (posn 0 0) 1 1)
+                    (motion 1 0 0 0 0 0)
+                    (list (rect (posn 1 0) 1 1)))
+              (rect (posn 0 0) 1 1))
