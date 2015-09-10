e623dbcb669c785ff5b5c4d2156a279043a175aa
Sam Caldwell <samcaldwell@utexas.edu>
2015-09-10 12:06:00 -0400

    cron commit

--- require_periodic_timer.rkt
+++ require_periodic_timer.rkt
@@ -1,9 +1,4 @@
 #lang prospect
 
-;; every shape process registers with the world by asserting ('register label shape)
-;; a shape can move by sending the message ('move label dx dy)
-;; the game logic process listens for movements and asserts the actual locations
-;; a rendering process listens for the actual locations and draws the map accordingly
-
 (require "./periodic_timer.rkt")
 
