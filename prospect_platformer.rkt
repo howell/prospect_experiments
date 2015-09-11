16bfbb01fb57bf00f9c8beaccf4ff2c6f1cb8a66
Sam Caldwell <samcaldwell@utexas.edu>
2015-09-11 18:12:00 -0400

    cron commit

--- prospect_platformer.rkt
+++ prospect_platformer.rkt
@@ -397,6 +397,7 @@
   (send dc clear)
   (send dc set-text-mode 'solid)
   (send dc set-text-foreground color)
+  (send dc set-scale 100 100)
   (send dc draw-text text (/ (posn-x bot-right) 4) (/ (posn-y bot-right) 2))
   (send dc resume-flush))
 
