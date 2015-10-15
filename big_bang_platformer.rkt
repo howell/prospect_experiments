4f6a920fbadd4e26a5f175e32cf69dab6c9e4f95
Sam Caldwell <samcaldwell@utexas.edu>
2015-10-15 11:08:00 -0400

    cron commit

--- big_bang_platformer.rkt
+++ big_bang_platformer.rkt
@@ -5,3 +5,22 @@
          (only-in "./geometry.rkt"
                     rect posn))
 
+;; num * num * num
+(struct motion (dx dy ddy) #:transparent)
+
+;; rect * motion
+(struct player (rect motion) #:transparent)
+
+;; rect  * (posn -> (dx * dy))
+(struct enemy (rect behavior) #:transparent)
+
+;; player * (setof rect) * rect * (listof enemy)
+(struct game-state (player env goal enemies) #:transparent)
+
+;; game-state -> game-state
+;; move the player
+;; move the enemies
+;; check if the player or enemies died
+;; check if the player reached the goal
+(define (timer-tick gs)
+  gs)
