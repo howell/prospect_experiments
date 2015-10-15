dd1766ab083d38a60c201a844d03b09c21fbb9ca
Sam Caldwell <samcaldwell@utexas.edu>
2015-10-15 10:52:00 -0400

    cron commit

--- big_bang_platformer.rkt
+++ big_bang_platformer.rkt
@@ -1,7 +1,8 @@
 #lang racket
 
+(require (only-in "./geometry.rkt"
+                    rect posn))
+
 (require 2htdp/universe
-         2htdp/image
-         (except-in "./geometry.rkt"
-                    circle))
+         2htdp/image)
 
