a0be592f80fa4f2b94d64ffadbcac762310a636e
Sam Caldwell <samcaldwell@utexas.edu>
2015-10-15 11:48:01 -0400

    cron commit

--- big_bang_platformer.rkt
+++ big_bang_platformer.rkt
@@ -32,10 +32,14 @@
   (match-define (player r p0-motion) player0)
   (match-define (motion dx dy ddy) p0-motion)
   (match-define (cons player1 y-col?) (move-player-y player0 dy env))
+  (define player-falling? (positive? dy))
   (define enemies1
-    (if (positive? dy)
+    (if player-falling?
         (filter (lambda (e) (not (overlapping-rects? player1 (enemy-rect e)))) enemies0)
         (enemies0)))
+  (when (and (not player-falling?)
+             (ormap (lambda (e) (overlapping-rects? player1 (enemy-rect e)))))
+    (error "player dead"))
   (match-define (cons player2 x-col?) (move-player-x player1 dx env))
   gs)
 
