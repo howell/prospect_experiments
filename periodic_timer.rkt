96a37acd81bd79dc4f64fff9c9f306fbd77a082d
Sam Caldwell <samcaldwell@utexas.edu>
2015-10-27 15:32:00 -0400

    cron commit

--- periodic_timer.rkt
+++ periodic_timer.rkt
@@ -9,13 +9,14 @@
 (define (periodically period-ms thunk)
   (define id (gensym 'after))
   (define set-timer-message (message (set-timer id period-ms 'relative)))
-  (list (spawn/stateless (lambda (e)
-                           (if (message? e)
-                               (let ([x (thunk)])
-                                 (cond
-                                   [(action? x) (list x set-timer-message)]
-                                   [(and (list? x) (andmap action? x)) (cons set-timer-message x)]
-                                   [else (list set-timer-message)]))
-                               #f))
-                         (sub (timer-expired id ?)))
-        set-timer-message))
+  (list (spawn/stateless
+         (lambda (e)
+           (and (message? e)
+                (define x (thunk))
+                (cond
+                  [(action? x) (list x set-timer-message)]
+                  [(and (list? x) (andmap action? x))
+                   (cons set-timer-message x)]
+                  [else (list set-timer-message)]))))
+        (sub (timer-expired id ?)))
+  set-timer-message))
